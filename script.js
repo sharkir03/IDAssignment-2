d46b4f7c601c7197c0591f7eb8526e71cc0b0226
sharkir03 <s10204929@connect.np.edu.sg>
2021-01-09 17:58:17 +0800

    Remove files, Print Weather API

--- script.js
+++ script.js
@@ -82,3 +82,27 @@
       
     }
   });//end doc ready
+
+//Weather Section
+var weather = document.querySelector('.load');
+var name = document.querySelector('.name');
+var desc = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
+
+weather.addEventListener('click',function(){
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=Singapore,sg&appid=228e17b1d67eda87fcbe05ade9e1245d')
+  .then(response => response.json())
+  .then(data => {
+    var nameValue = data['name'];
+    var tempValue = data['main']['temp'];
+    var descValue = data['weather'][0]['description'];
+
+    name.innerHTML = nameValue;
+    temp.innerHTML = 'Temperature: ' + Math.round(((tempValue -273.15)*100)/100) + '°C';
+    desc.innerHTML = 'Description: ' + descValue;
+  })
+
+.catch(err => alert("Wrong City Name!"))
+})
+
+
